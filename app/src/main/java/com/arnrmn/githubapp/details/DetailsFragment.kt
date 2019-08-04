01b68aaa668371f24f0b019b733f2b9966d07a1c
Arnoldas Armonas <arnrmn@telesoftas.com>
2019-08-04 14:47:04 +0300

    Add base for navigation

---
+++ app/src/main/java/com/arnrmn/githubapp/details/DetailsFragment.kt
@@ -0,0 +1,8 @@
+package com.arnrmn.githubapp.details
+
+import com.arnrmn.githubapp.R
+import com.arnrmn.githubapp.utils.android.BaseFragment
+
+class DetailsFragment : BaseFragment() {
+    override val layoutId = R.layout.fragment_details
+}
---
+++ app/src/main/java/com/arnrmn/githubapp/repositories/RepositoriesFragment.kt
@@ -0,0 +1,9 @@
+package com.arnrmn.githubapp.repositories
+
+import com.arnrmn.githubapp.R
+import com.arnrmn.githubapp.utils.android.BaseFragment
+
+class RepositoriesFragment : BaseFragment() {
+    override val layoutId = R.layout.fragment_repositories
+
+}
